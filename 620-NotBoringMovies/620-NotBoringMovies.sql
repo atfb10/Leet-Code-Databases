b591cc4d5b87a226176726860b5a19bf2253f8ac
Adam Forestier <aforestier10@gmail.com>
2018-08-30 23:22:16 -0500

    Improve file structure

--- 620-NotBoringMovies/620-NotBoringMovies.sql
+++ 620-NotBoringMovies/620-NotBoringMovies.sql
@@ -10,7 +10,7 @@ For example, table cinema:
 |   1     | War       |   great 3D   |   8.9     |
 |   2     | Science   |   fiction    |   8.5     |
 |   3     | irish     |   boring     |   6.2     |
-|   4     | Ice song  |   Fantacy    |   8.6     |
+|   4     | Ice song  |   Fantasy    |   8.6     |
 |   5     | House card|   Interesting|   9.1     |
 +---------+-----------+--------------+-----------+
 For the example above, the output should be:
